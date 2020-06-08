6b169ddc456a8cf55f04313822a958ece168e3b7
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2020-06-08 20:58:22 -0300

    Create Front_End.java

---
+++ 7)/Front_End.java
@@ -0,0 +1,12 @@
+package heranca;
+public class Front_End extends Programador {
+    public void css() {
+        System.out.println("    *Fazendo CSS...");
+    }
+    public void construirUI() {
+        System.out.println("    *Construindo UI...");
+    }
+    public void tornarResponsivo() {
+        System.out.println("    *Tornando responsivo...");
+    }
+}
